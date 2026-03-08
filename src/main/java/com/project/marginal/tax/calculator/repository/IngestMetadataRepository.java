97981451648b9cd2ef1a9301c28d64b15beed32d
Hamza <olowuhamzat@gmail.com>
2026-03-08 12:23:28 -0400

    Remove unused

--- src/main/java/com/project/marginal/tax/calculator/repository/IngestMetadataRepository.java
+++ src/main/java/com/project/marginal/tax/calculator/repository/IngestMetadataRepository.java
@@ -4,8 +4,6 @@ import com.project.marginal.tax.calculator.entity.IngestMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface IngestMetadataRepository extends JpaRepository<IngestMetadata, Integer> {
     // single row, always id=1
